d87ccd0675960bb3fcbdb242e339334f39040c57
Rellium <relliumachine42@gmail.com>
2023-07-25 14:33:02 -0400

    described code

--- js/site.js
+++ js/site.js
@@ -1,12 +1,9 @@
 function getValues() {
     let userString = document.getElementById('userString').value;
-    // let revString = reverseString(userString);
     displayString(reverseString(userString));
 }
 
 function reverseString(input) {
-    // let strArray = input.split('');
-    // return strArray.reverse().join('');
     let revString = '';
     for (let i = input.length - 1; i >= 0; i--) {
         let letter = input[i];
@@ -17,8 +14,5 @@ function reverseString(input) {
 
 function displayString(output) {
     document.getElementById('results').textContent = output;
-
-    let alertBox = document.getElementById('alert');
-
-    alertBox.classList.remove('invisible');
+    document.getElementById('alert').classList.remove('invisible');
 }
