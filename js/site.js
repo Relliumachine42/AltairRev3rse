daac454a85592d33fecc05732eaeedd5389815c8
Rellium <relliumachine42@gmail.com>
2023-07-25 13:30:15 -0400

    added logo and js

--- js/site.js
+++ js/site.js
@@ -1,13 +1,24 @@
-function displayMessage() {
-    let inputBox = document.getElementById('message');
-    
-    let message = inputBox.value;
+function getValues() {
+    let userString = document.getElementById('userString').value;
+    // let revString = reverseString(userString);
+    displayString(reverseString(userString));
+}
 
-    Swal.fire(
-        {
-            backdrop: false,
-            title: 'App Name',
-            text: message
-        }
-    );
+function reverseString(input) {
+    // let strArray = input.split('');
+    // return strArray.reverse().join('');
+    let revString = '';
+    for (let i = input.length - 1; i >= 0; i--) {
+        let letter = input[i];
+        revString += letter;
+    }
+    return revString;
+}
+
+function displayString(output) {
+    document.getElementById('results').textContent = output;
+
+    let alertBox = document.getElementById('alert');
+
+    alertBox.classList.remove('invisible');
 }
